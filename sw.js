5b5a2688a9e75d361ffda54a12ec947c3ec4dc4e
ansarimofid <ansarimofid>
2018-01-14 01:10:51 +0530

    Adds service worker

---
+++ sw.js
@@ -0,0 +1,70 @@
+/*
+ Service Worker implementation
+ inspired from https://github.com/GoogleChromeLabs/airhorn/blob/master/app/sw.js
+ */
+
+let version = '1.4.0';
+
+let staticCacheName = 'mws-rrs1-' + version;
+
+
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+
+/*
+ * Adapted from https://developers.google.com/web/ilt/pwa/lab-caching-files-with-service-worker
+ * Another way to cache is to cache it in 'install' event, but I am not sure if rubrics demands that
+ * It says visited page should show when there is no network access so only caching requests as they happen
+ */
+self.addEventListener('fetch', function(event) {
+  console.log('Fetch event for ', event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request)
+        .then(function(response) {
+          // TODO 5 - Respond with custom 404 page
+          return caches.open(staticCacheName).then(function(cache) {
+            if (event.request.url.indexOf('maps') < 0) { // don't cache google maps
+              // ^ it's not a site asset, is it?
+              console.log('Saving ' + event.request.url + ' into cache.');
+              cache.put(event.request.url, response.clone());
+            }
+            return response;
+          });
+        });
+
+    }).catch(function(error) {
+      // TODO 6 - Respond with custom offline page
+    })
+  );
+});
+
+
+/* delete old cache */
+self.addEventListener('activate', function(event) {
+  console.log('Activating new service worker...');
+
+  var cacheWhitelist = [staticCacheName];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
